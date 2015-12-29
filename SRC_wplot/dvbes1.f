ee03a4d78f14ee6f91591c75611c82e8cea954b5
Elias Assmann <elias.assmann@gmail.com>
2015-12-29 19:17:33 +0100

    SRC_wplot/dvbes1.f cleanup
     * use module `param` instead of `const`
    
     * use `DPk` instead of `R8`
    
     * use `implicit none`
    
     * remove unused argument

--- SRC_wplot/dvbes1.f
+++ SRC_wplot/dvbes1.f
@@ -1,37 +1,42 @@
 !!! wien2wannier/SRC_wplot/dvbes1.f
 
-      SUBROUTINE DVBES1(FJ,DJ,SM,RI,NT)                                 
+subroutine DVBES1(FJ,DJ,SM,NT)
 !-----X----X----X----X----X----X----X----X----X----X----X----X----X----X
 !-----X CALCULATE THE DERIVATIVES OF THE BESSEL FUNCTIONS.   X----X----X
 !-----X   DJ=DFJ/DX WHERE X=SM*RI                                 X----X
 !-----X                    D.D.KOELLING                      X----X----X
 !-----X----X----X----X----X----X----X----X----X----X----X----X----X----X
-      use const
-      IMPLICIT REAL(R8) (A-H,O-Z)
-      DIMENSION DJ(*),FJ(*)
-      DATA ZUP/1.0D-5/,ZERO/0.0D0/,THIRD/0.3333333333333D0/,ONE/1.0D0/
+  use param, only: DPk
 
-      dummy = ri                ! silence warning
+  implicit none
 
-      X=SM
-      IF(X.GT.ZUP) GOTO 20
-      DJ(1)=ZERO
-      DJ(2)=THIRD
-      DO 10 L=3,NT
-         DJ(L)=ZERO
- 10   CONTINUE
-      RETURN
-   20 Q2=-ONE/X
-      Q3=Q2
-      DJ(1)=-FJ(2)
-      LM=1
-      DO 30 L=2,NT
-      Q3=Q3+Q2
-      DJ(L)=FJ(LM)+Q3*FJ(L)
-      LM=LM+1
-   30 CONTINUE
-      RETURN
-      END
+  real(DPk), intent(in)  :: FJ(*), SM
+  real(DPk), intent(out) :: DJ(*)
+  integer,   intent(in)  :: NT
+
+  real(DPk), parameter :: ZUP = 1e-5_DPk
+
+  integer   :: l, lm
+  real(DPk) :: Q2, Q3
+
+  if(SM <= ZUP) then
+     DJ(1) = 0
+     DJ(2) = 1/3._DPk
+     do L=3,NT
+        DJ(L) = 0
+     end do
+  else
+     Q2=-1/SM
+     Q3=Q2
+     DJ(1)=-FJ(2)
+     LM=1
+     do L=2,NT
+        Q3=Q3+Q2
+        DJ(L)=FJ(LM)+Q3*FJ(L)
+        LM=LM+1
+     end do
+  end if
+end subroutine DVBES1
 
 
 !!/---
@@ -40,4 +45,4 @@
 !! End:
 !!\---
 !!
-!! Time-stamp: <2015-05-23 19:58:48 elias>
+!! Time-stamp: <2015-12-28 14:00:57 assman@faepop36.tu-graz.ac.at>
