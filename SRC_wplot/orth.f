04ad5e45c827551e86149870b1e3917fc4ee5b42
Elias Assmann <elias.assmann@gmail.com>
2015-12-29 19:34:51 +0100

    SRC_wplot/orth.f cleanup
     * use `DPk` instead of `R8`
    
     * use module `param` instead of `const`
    
     * use `implicit none`

--- SRC_wplot/orth.f
+++ SRC_wplot/orth.f
@@ -1,19 +1,25 @@
 !!! wien2wannier/SRC_wplot/orth.f
 
 LOGICAL FUNCTION ORTH(A)
-  use const
-  IMPLICIT REAL(R8) (A-H,O-Z) 
-  DIMENSION A(3,3)
+  use param, only: DPk
+  implicit none
+
+  real(DPk), intent(in) :: A(3,3)
+
+  real(DPk), parameter  :: tol = 13-4_DPk
+
+  integer   :: i, j
+  real(DPk) :: T
+
   !
   ! Function to check whether A is orthogonal or not
   ! 
-  DATA TOL / 1.0D-4 /
 
   ORTH = .FALSE.
   DO I=1,3
      DO J=1,3
         T = A(1,I)*A(1,J) + A(2,I)*A(2,J) + A(3,I)*A(3,J)
-        IF(J.EQ.I) T = T - 1.0D0
+        IF(J.EQ.I) T = T - 1
         IF(ABS(T).GT.TOL) RETURN
      END DO
   END DO
@@ -28,4 +34,4 @@ END FUNCTION ORTH
 !! End:
 !!\---
 !!
-!! Time-stamp: <2015-05-23 19:58:48 elias>
+!! Time-stamp: <2015-12-29 18:40:12 assman@faepop36.tu-graz.ac.at>
