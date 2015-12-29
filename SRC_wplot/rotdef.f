64019a62b7333194deb547d5a3058dd1cd602bdf
Elias Assmann <elias.assmann@gmail.com>
2015-12-29 19:49:07 +0100

    SRC_wplot/rotdef.f cleanup
     * use `DPk` instead of `R8`
    
     * use `implicit none`
    
     * use `struct_t` argument

--- SRC_wplot/rotdef.f
+++ SRC_wplot/rotdef.f
@@ -1,19 +1,27 @@
 !!! wien2wannier/SRC_wplot/rotdef.f
 
-SUBROUTINE ROTDEF(NAT,MULT,POS,IOP)
-  use const
-  use param
-  use latt
-  use sym2
-  IMPLICIT REAL(R8) (A-H,O-Z)
-  DIMENSION MULT(NAT), POS(3,Nat*48), IOP(Nat*48)
-!
+subroutine rotdef(stru, IOP)
+  use structmod, only: struct_t
+  use clio,      only: croak
+  use param,     only: unit_out, mvatom, DPk
+  use latt,      only: br2
+  use struct,    only: pos
+  use sym2,      only: imat, tau, iord
+
+  implicit none
+
+  type(struct_t), intent(in)  :: stru
+  integer,        intent(out) :: IOP(stru%Nneq*48)
+
+  real(DPk), parameter :: half = 0.5_DPk, tol = 1e-4_DPk
+
+  integer   :: index, index1, i, j, jatom, jOp, m
+  real(DPk) :: pos0(3), R(3), D(3)
+
 ! << Input >>
-! NAT      -- number of inequivalent basis atoms
-! MULT(j)  -- number of equivalent atom within the j-th set of inequiv. atoms
-! POS(:,i) -- primitive fractional coordinates of the i-th atom 
+! stru     -- struct
 !
-! from COMMON /SYM2/ -- symmetry operations
+! from MODULE SYM2 -- symmetry operations
 ! IMAT,TAU -- symmetry operations (in primitive fractional coordinates)
 ! IORD     -- number of symmetry operations
 ! ----------------------------------------------------------------------------
@@ -28,21 +36,18 @@ SUBROUTINE ROTDEF(NAT,MULT,POS,IOP)
 ! Caution: The symmetry operations selected here to generate the individual 
 !          atoms must precisely co-incide with those selected in LAPW2 !
 
-  DIMENSION POS0(3),R(3),D(3)
-  !
-  DATA TOL /1.0D-4/ , ONE,HALF /1.0D0, 0.5D0/
 !
 !     << find the generating symmetry operations >>
       WRITE(unit_out,2000)
 
       INDEX=0
-      DO 10 JATOM=1,NAT
+      DO 10 JATOM=1,STRU%NNEQ
         INDEX1 = INDEX+1
 !       << store r_1.atom before updating it >>
         DO 15 J=1,3
-          POS0(J) = POS(J,INDEX1)
+          POS0(J) = STRU%POS(J,INDEX1)
    15   CONTINUE
-        DO 20 M=1,MULT(JATOM)
+        do 20 M=1,stru%mult(jatom)
           INDEX = INDEX+1
           DO 30 JOP=1,IORD
 !
@@ -55,14 +60,15 @@ SUBROUTINE ROTDEF(NAT,MULT,POS,IOP)
 !
 !           << check the difference modulo lattice translations >>
             DO 50 I=1,3
-              D(I) = ABS(MOD(ABS(POS(I,INDEX)-R(I))+HALF,ONE)-HALF)
+               D(I) = abs(mod(abs(stru%pos(i,index)-R(I)) + HALF, 1._DPk) &
+                    - HALF)
    50       CONTINUE
             IF (D(1).LT.TOL.AND.D(2).LT.TOL.AND.D(3).LT.TOL) THEN
               IOP(INDEX) = JOP
 !:17[
               IF(.NOT.MVATOM)THEN
                 DO I=1,3
-                   R(I) = POS(I,INDEX)
+                   R(I) = STRU%POS(I,INDEX)
                 END DO
               ENDIF
 !:17]
@@ -85,7 +91,7 @@ SUBROUTINE ROTDEF(NAT,MULT,POS,IOP)
           WRITE(unit_out,1000) INDEX1,INDEX
           WRITE(unit_out,1010) INDEX1,(POS(I,INDEX1),I=1,3)
           WRITE(unit_out,1010) INDEX ,(POS(I,INDEX ),I=1,3)
-          STOP 'ROTDEF'
+          call croak('error in ROTDEF')
 !
  20     CONTINUE
  10   CONTINUE
@@ -111,4 +117,4 @@ SUBROUTINE ROTDEF(NAT,MULT,POS,IOP)
 !! End:
 !!\---
 !!
-!! Time-stamp: <2015-05-23 19:58:48 elias>
+!! Time-stamp: <2015-12-29 19:48:03 assman@faepop36.tu-graz.ac.at>
