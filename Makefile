1dcda8d7a241910847f3e20fa22766c77e037c30
Elias Assmann <elias.assmann@gmail.com>
2016-04-08 16:41:51 +0200

    header.tex: update links for wien2wannier, woptic

--- Makefile
+++ Makefile
@@ -6,7 +6,7 @@
 
 VERSION := $(shell git describe)
 ifeq "$(VERSION)" ""
-VERSION = $(lastword '$version: v1.0.0-107-g4e08cd0$')
+VERSION = $(lastword '$version: v1.0.0-108-g05d8e80$')
 endif
 
 SIMPLE      := SRC_trig doc
--- SRC_wplot/moduls.f
+++ SRC_wplot/moduls.f
@@ -7,7 +7,7 @@ module param
 
   public
 
-  character(*), parameter, private :: rev_str="$version: v1.0.0-107-g4e08cd0$"
+  character(*), parameter, private :: rev_str="$version: v1.0.0-108-g05d8e80$"
   character(*), parameter, public  :: &
        wplot_version = rev_str(11 : len (rev_str)-1)
 
