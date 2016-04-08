4e08cd07425e7cc654ca46c10c8ec901785965e0
Elias Assmann <elias.assmann@gmail.com>
2016-04-08 16:17:04 +0200

    UG: update references
     * unify CPC → Comput. Phys. Comm.
     * add links to CPC in introduction

--- Makefile
+++ Makefile
@@ -6,7 +6,7 @@
 
 VERSION := $(shell git describe)
 ifeq "$(VERSION)" ""
-VERSION = $(lastword '$version: v1.0.0-105-g86921a1$')
+VERSION = $(lastword '$version: v1.0.0-106-g727c032$')
 endif
 
 SIMPLE      := SRC_trig doc
--- SRC_wplot/moduls.f
+++ SRC_wplot/moduls.f
@@ -7,7 +7,7 @@ module param
 
   public
 
-  character(*), parameter, private :: rev_str="$version: v1.0.0-105-g86921a1$"
+  character(*), parameter, private :: rev_str="$version: v1.0.0-106-g727c032$"
   character(*), parameter, public  :: &
        wplot_version = rev_str(11 : len (rev_str)-1)
 
