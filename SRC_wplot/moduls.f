3344cb8be02c81087301c21223fbbb1e48036d5b
Elias Assmann <elias.assmann@gmail.com>
2016-07-05 17:29:47 +0200

    SRC_w2w/inverss.f, SRC_w2w/ph.f: remove (unneeded)

--- SRC_wplot/moduls.f
+++ SRC_wplot/moduls.f
@@ -7,7 +7,7 @@ module param
 
   public
 
-  character(*), parameter, private :: rev_str="$version: v1.0.0-118-gfc9d4b0$"
+  character(*), parameter, private :: rev_str="$version: v1.0.0-119-g27a1e2c$"
   character(*), parameter, public  :: &
        wplot_version = rev_str(11 : len (rev_str)-1)
 
