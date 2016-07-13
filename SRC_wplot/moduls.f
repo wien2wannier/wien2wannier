87270c638e6a7f5abc20c0ab9e718adf48ea17f8
Elias Assmann <elias.assmann@gmail.com>
2016-07-13 18:14:12 +0200

    test/PoC/SrVO3-PoC/SrVO3-PoC.sh: fix w2wdir

--- SRC_wplot/moduls.f
+++ SRC_wplot/moduls.f
@@ -7,7 +7,7 @@ module param
 
   public
 
-  character(*), parameter, private :: rev_str="$version: v1.0.0-126-g0320e17$"
+  character(*), parameter, private :: rev_str="$version: v1.0.0-128-gabf9886$"
   character(*), parameter, public  :: &
        wplot_version = rev_str(11 : len (rev_str)-1)
 
