feadd1cb9c7d15dfc6a436016e99033c10bfadd6
Elias Assmann <elias.assmann@gmail.com>
2016-12-06 17:44:31 +0100

    update NEWS for last commit

--- SRC_w2w/mod_w2w.f
+++ SRC_w2w/mod_w2w.f
@@ -28,7 +28,7 @@ module param
   public
 
   character(*), parameter, private :: &
-       rev_str = "$version: v1.0.0-278-g35a7faa$"
+       rev_str = "$version: v1.0.0-278-g4f639f5$"
   character(*), parameter, public  :: &
        wien2wannier_version = rev_str(11 : len (rev_str)-1)
 
