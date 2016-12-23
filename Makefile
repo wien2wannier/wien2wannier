641b6515707ff1b16960c9e18559c2143cf61f14
Elias Assmann <elias.assmann@gmail.com>
2016-12-23 09:43:52 +0100

    make.sys.example: add -ip for ifort

--- Makefile
+++ Makefile
@@ -8,7 +8,7 @@
 
 SHELL=/bin/bash
 
-version = $(lastword '$version: v2.0.0-4-g8301698$')
+version = $(lastword '$version: v2.0.0-5-g17fc22a$')
 
 VERSION = $(shell git describe 2>/dev/null || echo $(version))
 
--- SRC_w2w/mod_w2w.f
+++ SRC_w2w/mod_w2w.f
@@ -28,7 +28,7 @@ module param
   public
 
   character(*), parameter, private :: &
-       rev_str = "$version: v2.0.0-4-g8301698$"
+       rev_str = "$version: v2.0.0-5-g17fc22a$"
   character(*), parameter, public  :: &
        wien2wannier_version = rev_str(11 : len (rev_str)-1)
 
